1ca4a634aa09eaf015779f857103af34f9e8f6c8
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-12 19:23:34 +0300

    navbar bg

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,21 +2,22 @@ import Link from 'next/link'
 import React from 'react'
 import {FaTwitter, FaGithub} from 'react-icons/fa'
 import ThemeToggleBtn from './ThemeToggleBtn'
+import { Home, HomeIcon } from 'lucide-react'
 
 export default function Navbar() {
 
   return (
-    <nav className='bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10'>
+    <nav className=' p-4 sticky bg-background top-0 drop-shadow-xl z-10 border-b border-2'>
         <div className='md:px-6 prose prose-xl mx-auto flex justify-between items-center'>
-            <h1 className='text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0'>
-              <Link href='/' className='text-white/90 no-underline hover:text-white'>Douglas Kipyegon</Link>
+            <h1 className='text-3xl font-bold grid place-content-center mb-2 md:mb-0'>
+              <Link href='/' className='flex gap-1 items-center no-underline text-slate-700 dark:text-slate-300 hover:opacty/90'><span className='hidden md:inline-flex'>Douglas Kipyegon</span><HomeIcon  /></Link>
             </h1>
-            <div className='flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl'>
-                <Link className='text-white/90 hover:text-white' href='https://github.com/kippytech'>
-                    <FaGithub size={30} />
+            <div className='flex flex-row items-center gap-4  text-4xl lg:text-5xl'>
+                <Link className='' href='https://github.com/kippytech'>
+                    <FaGithub title='github icon' className='text-slate-700 dark:text-slate-300 hidden md:inline-flex' size={30} />
                 </Link>
                 <Link className='text-white/90 hover:text-white' href='https://twitter.com/kipyegosancho'>
-                    <FaTwitter size={30} />
+                    <FaTwitter title='twitter aka X icon' className='text-slate-700 dark:text-slate-300 hidden md:inline-flex' size={30} />
                 </Link>
                 <ThemeToggleBtn />
             </div>
